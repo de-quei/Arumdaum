97838223d5d4b9a69a2fb5820a5f8903f630ac6c
Hyunji Kim <papertoy01@gmail.com>
2023-08-15 23:14:28 +0900

    [✨] LibraryFrame에 MainHeader이미지 추가

--- ArumDaum/src/frame/LibraryFrame.java
+++ ArumDaum/src/frame/LibraryFrame.java
@@ -1,15 +1,26 @@
 package frame;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class LibraryFrame extends JFrame{
 
+	//배경 이미지 상대경로
+    JLabel MainHeader = new JLabel(new ImageIcon("./img/MainHeader.png"));
+    
 	public LibraryFrame() {
+		// 창 초기설정
 		setSize(1280, 750);
         setResizable(false);
         setTitle("아름다움");
         setLayout(null);
         setLocationRelativeTo(null);
+        
+        // 이미지의 위치, 크기
+        MainHeader.setBounds(0, 0, 1280, 48);
+        add(MainHeader);
+        
         setVisible(true);
 	}
     
