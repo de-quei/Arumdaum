b268759e874a77360af36407585580020c9b0c3b
Hyunji Kim <papertoy01@gmail.com>
2023-08-23 22:48:08 +0900

    [♻️] import 정리

--- ArumDaum/src/frame/LoginFrame.java
+++ ArumDaum/src/frame/LoginFrame.java
@@ -1,22 +1,9 @@
 package frame;
 
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.*;
 
 public class LoginFrame extends JFrame {
 
