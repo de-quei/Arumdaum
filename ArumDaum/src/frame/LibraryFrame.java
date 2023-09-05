e6218a40337d9e58aa85f712dbc77ac6d440d3a9
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-09-05 09:06:03 +0900

    [✨] panel에 label 추가

--- ArumDaum/src/frame/LibraryFrame.java
+++ ArumDaum/src/frame/LibraryFrame.java
@@ -1,9 +1,12 @@
 package frame;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 import javax.swing.border.LineBorder;
 
 public class LibraryFrame extends JFrame{
@@ -29,6 +32,13 @@ public class LibraryFrame extends JFrame{
 		header.setSize(1280, 48);
 		header.setLocation(0, 0);
 		header.setBorder(new LineBorder(Color.BLACK));
+		
+		JLabel header_label = new JLabel("\"아름다움\"");
+		header_label.setFont(new Font("Gowun Batang", Font.PLAIN, 25));
+		header_label.setHorizontalTextPosition(SwingConstants.CENTER);
+		header_label.setBounds(0, 0, 1280, 48);
+		header.add(header_label);
+		
 		add(header);
 	}
 }
