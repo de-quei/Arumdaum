311bc6bfe33ff6e7b4c17a39e90fd2dc782e943c
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-09-12 09:58:22 +0900

    [✨] ManagerFrame 초기설정

--- ArumDaum/src/frame/ManagerFrame.java
+++ ArumDaum/src/frame/ManagerFrame.java
@@ -1,5 +1,20 @@
 package frame;
 
-public class ManagerFrame {
+import javax.swing.JFrame;
 
+public class ManagerFrame extends JFrame{
+
+	public ManagerFrame() {
+        initailzeUI();
+        setVisible(true);
+    }
+
+	private void initailzeUI() {
+		setSize(1280, 750);
+        setResizable(false);
+        setTitle("관리자 화면");
+        setLayout(null);
+        setLocationRelativeTo(null);
+	}
+	
 }
