b8891ceedce307c878a3905bb558e27f8d4a5116
Hyunji Kim <s2208@e-mirim.hs.kr>
2023-11-07 11:00:31 +0900

    매니저 프레임 / 신청 현황에 JTable로 데이터 출력 및 수락 버튼 생성

--- ArumDaum/src/Manager/MApplicationStatusFrame.java
+++ ArumDaum/src/Manager/MApplicationStatusFrame.java
@@ -1,44 +1,78 @@
 package Manager;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
 
 public class MApplicationStatusFrame extends JFrame{
 	
+	JPanel borderPanel = new JPanel(); // border를 넣을 panel
 	JPanel buttonPanel = new JPanel(); // button을 넣을 panel
 	
 	JButton backBtn = new JButton("이전으로");
+	JButton checkBtn = new JButton("수락");
 	
-	public MApplicationStatusFrame(){
+	// JTable 모델
+	String[] header = {"순번", "도서명", "카테고리"}; // 중간에 신청일자..
+	DefaultTableModel tableModel = new DefaultTableModel(header, 0);
+	JTable statusTable = new JTable(tableModel);
+
+	public MApplicationStatusFrame() {
 		initializeUI();
 		addComponentsUI();
 		eventHandler();
+		loadApplicationStatus();
 		setVisible(true);
 	}
-
+	
+	
 	private void initializeUI() {
-		// 창 초기설정
+        // 창 초기설정
         setSize(500, 500);
         setResizable(false);
         setTitle("아름다움");
         setLayout(null);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    }
 	
 	private void addComponentsUI() {
-		//button panel
-      	buttonPanel.setBounds(20, 410, 440, 40);
-      	add(buttonPanel);
-      	
-      	//panel에 이전버튼 생성
-      	backBtn.setBounds(350, 0, 90, 40);
+		borderPanel.setBounds(20, 20, 440, 380);
+		borderPanel.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "신청 현황"));
+		borderPanel.setLayout(null);
+		add(borderPanel);
+		
+		buttonPanel.setBounds(20, 410, 440, 40);
+		//buttonPanel.setBorder(new LineBorder(Color.BLACK));
+		add(buttonPanel);
+		
+		JScrollPane tableScrollPane = new JScrollPane(statusTable);
+        tableScrollPane.setBounds(20, 40, 400, 310);
+        borderPanel.add(tableScrollPane);
+        
+        //panel에 이전버튼 생성
+        checkBtn.setBounds(350, 0, 90, 40);
         buttonPanel.setLayout(null);
-      	buttonPanel.add(backBtn);
+        buttonPanel.add(checkBtn);
+        
+        backBtn.setBounds(0, 0, 90, 40);
+        buttonPanel.setLayout(null);
+        buttonPanel.add(backBtn);
 	}
 	
 	private void eventHandler() {
@@ -46,13 +80,45 @@ public class MApplicationStatusFrame extends JFrame{
 		backBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				openMChooseFrame();
+				openChooseFrame();
 			}
 		});
 	}
 	
-	private void openMChooseFrame() {
+	private void openChooseFrame() {
 		dispose();
 		new MChooseFrame();
 	}
+	
+	private void loadApplicationStatus() {
+        // 데이터베이스 연결 정보
+		String DB_URL = "jdbc:mysql://localhost:3306/arumdaum?useTimezone=true&serverTimezone=UTC";
+        String DB_USER = "root";
+        String DB_PASSWORD = "0000";
+
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+            Statement statement = connection.createStatement();
+            String selectQuery = "SELECT * FROM bookApplication";
+
+            ResultSet resultSet = statement.executeQuery(selectQuery);
+
+            // JTable 모델 초기화
+            tableModel.setRowCount(0);
+
+            // 결과셋을 JTable에 추가
+            while (resultSet.next()) {
+                String[] rowData = {
+                		//TODO:null은 나중에 구현할 것
+                        resultSet.getString("id"),
+                        resultSet.getString("title"),
+                        //resultSet.getString(null),
+                        resultSet.getString("category"),
+                        //resultSet.getString(null)
+                };
+                tableModel.addRow(rowData);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- ArumDaum/src/Manager/MBookStatusFrame.java
+++ ArumDaum/src/Manager/MBookStatusFrame.java
@@ -1,44 +1,103 @@
 package Manager;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
 
 public class MBookStatusFrame extends JFrame{
-	
+
+	JPanel borderPanel = new JPanel(); // border를 넣을 panel
 	JPanel buttonPanel = new JPanel(); // button을 넣을 panel
 	
-	JButton backBtn = new JButton("이전으로");
+	JButton backBtn = new JButton("이전으로"); // 이전 버튼
+	JButton searchBtn = new JButton("검색");
 	
-	public MBookStatusFrame(){
+	JLabel searchLabel = new JLabel("도서 검색 : ");
+	
+	JTextField searchField = new JTextField(); // 도서 검색 바
+	
+	//임시데이터입니다.
+    DefaultTableModel tableModel;
+    JTable resultTable;
+    
+    String[] header = {"번호", "도서명", "ISBN", "출판사", "저자", "수량"};
+    String[][] contents = {};
+    
+    String dbUrl = "jdbc:mysql://localhost:3306/arumdaum?useTimezone=true&serverTimezone=UTC";
+    String dbUser = "root";
+    String dbPassword = "0000";
+	
+	public MBookStatusFrame() {
 		initializeUI();
 		addComponentsUI();
 		eventHandler();
+		loadDataFromDatabase();
 		setVisible(true);
 	}
-
+	
 	private void initializeUI() {
-		// 창 초기설정
+        // 창 초기설정
         setSize(500, 500);
         setResizable(false);
         setTitle("아름다움");
         setLayout(null);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+    }
 	
 	private void addComponentsUI() {
-		//button panel
-      	buttonPanel.setBounds(20, 410, 440, 40);
-      	add(buttonPanel);
-      	
-      	//panel에 이전버튼 생성
-      	backBtn.setBounds(350, 0, 90, 40);
+		
+		borderPanel.setBounds(20, 20, 440, 380);
+		borderPanel.setBorder(new TitledBorder(new LineBorder(Color.BLACK), "도서 현황"));
+		borderPanel.setLayout(null);
+		add(borderPanel);
+		
+		buttonPanel.setBounds(20, 410, 440, 40);
+		//buttonPanel.setBorder(new LineBorder(Color.BLACK));
+		add(buttonPanel);
+		
+		//검색 바
+		searchLabel.setBounds(30, 40, 80, 25);
+		searchField.setBounds(120, 40, 230, 25);
+		searchBtn.setBounds(360, 40, 60, 25);
+		borderPanel.add(searchLabel);
+		borderPanel.add(searchField);
+		borderPanel.add(searchBtn);
+        
+		// JTable을 DefaultTableModel로 초기화합니다.
+	    tableModel = new DefaultTableModel(contents, header);
+
+	    // JTable을 DefaultTableModel로 설정합니다.
+	    resultTable = new JTable(tableModel);
+
+	    // JScrollPane을 사용하여 JTable을 감싸기
+	    JScrollPane tableScrollPane = new JScrollPane(resultTable);
+	    tableScrollPane.setBounds(30, 90, 390, 270);
+	    borderPanel.add(tableScrollPane);
+		
+		//panel에 이전버튼 생성
+		backBtn.setBounds(350, 0, 90, 40);
+        
         buttonPanel.setLayout(null);
-      	buttonPanel.add(backBtn);
+		buttonPanel.add(backBtn);
 	}
 	
 	private void eventHandler() {
@@ -46,13 +105,85 @@ public class MBookStatusFrame extends JFrame{
 		backBtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				openMChooseFrame();
+				openChooseFrame();
 			}
+		});
+		//검색버튼
+		searchBtn.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		        String searchText = searchField.getText().trim();
+		        if (!searchText.isEmpty()) {
+		            searchBooksByTitle(searchText);
+		        }
+		    }
 		});
 	}
 	
-	private void openMChooseFrame() {
+	private void openChooseFrame() {
 		dispose();
 		new MChooseFrame();
+	}
+	
+	private void loadDataFromDatabase() {
+        try {
+            Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+            Statement statement = connection.createStatement();
+            String query = "SELECT * FROM book"; 
+            ResultSet resultSet = statement.executeQuery(query);
+
+            // 테이블의 기존 데이터를 지웁니다.
+            tableModel.setRowCount(0);
+
+            // 데이터베이스에서 가져온 데이터로 JTable을 채웁니다.
+            while (resultSet.next()) {
+            	String bookId = resultSet.getString("bookID");
+                String bookName = resultSet.getString("title"); 
+                String isbn = resultSet.getString("ISBN");
+                String publisher = resultSet.getString("publisher");
+                String author = resultSet.getString("author");
+                String status = resultSet.getString("totalCopies");
+
+                // JTable에 새로운 행을 추가합니다.
+                tableModel.addRow(new String[]{bookId, bookName, isbn, publisher, author, status});
+            }
+
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	//TODO 검색 정확도 높이기! 무조건!
+	private void searchBooksByTitle(String title) {
+	    try {
+	        Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+	        Statement statement = connection.createStatement();
+	        String query = "SELECT * FROM book WHERE title LIKE '%" + title + "%'";
+	        ResultSet resultSet = statement.executeQuery(query);
+
+	        // 테이블의 기존 데이터를 지웁니다.
+	        tableModel.setRowCount(0);
+
+	        // 데이터베이스에서 가져온 데이터로 JTable을 채웁니다.
+	        while (resultSet.next()) {
+	            String bookName = resultSet.getString("title");
+	            String isbn = resultSet.getString("ISBN");
+	            String publisher = resultSet.getString("publisher");
+	            String author = resultSet.getString("author");
+	            String status = resultSet.getString("totalCopies");
+
+	            // JTable에 새로운 행을 추가합니다.
+	            tableModel.addRow(new String[]{bookName, isbn, publisher, author, status});
+	        }
+
+	        resultSet.close();
+	        statement.close();
+	        connection.close();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
 	}
 }
